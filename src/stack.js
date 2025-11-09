3500a276e7807424f61e4ceb681bba9c0c41a077
Nastassia Romanova <romanova7410020@gmail.com>
2025-11-09 18:01:56 +0300

    fix: add solution stack.js

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../lib/errors');
  *
  */
 class Stack {
-  push(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.item = [];
+  }
+  push(value) {
+    this.item.push(value);
   }
 
   pop() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.item.pop();
   }
 
   peek() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+   return this.item[this.item.length - 1];
   }
 }
 
